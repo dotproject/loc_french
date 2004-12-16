f82944ed85e9fbbb69061d327ce5f64e0a2e9736
ajdonnison <ajdonnison>
2004-12-16 01:18:44 +0000

    New language support file for 2.0

---
+++ lang.php
@@ -0,0 +1,5 @@
+<?php
+	$dir = basename(dirname(__FILE__));
+	$LANGUAGES['fr_FR'] = array($dir, 'French', 'Francais', 'fra');
+	$LANGUAGES['fr_CA'] = array($dir, 'French (Can)', 'Francais (Can)', 'frc');
+?>
